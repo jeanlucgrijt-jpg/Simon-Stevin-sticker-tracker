216abea78fcee9a5dadac58004d559746f93bba9
Jean-Luc Grijt <j.c.a.grijt@student.tue.nl>
2026-03-26 00:17:22 +0100

    CHAR limits update

--- Database/createDB_sticker_tracker.sql
+++ Database/createDB_sticker_tracker.sql
@@ -2,13 +2,13 @@ CREATE DATABASE sticker_tracker;
 USE sticker_tracker;
 
 CREATE TABLE sticker_data (
-    photo_id VARCHAR(500),
-    user_id  VARCHAR(80), 
+    photo_id VARCHAR(100),
+    user_id  VARCHAR(50), 
     latitude DECIMAL(9,6),
     longitude DECIMAL(9,6),
     date_picture DATE,
     date_uploaded DATE,
-    committee VARCHAR(80),
-    title VARCHAR(100),
+    committee VARCHAR(30),
+    title VARCHAR(50),
     description TEXT
 );
