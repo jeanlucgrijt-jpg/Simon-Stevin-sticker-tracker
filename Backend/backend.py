2530c390a3ccc77139fbc0c4964d55486eeb4a06
Jean-Luc Grijt <j.c.a.grijt@student.tue.nl>
2026-03-26 13:14:12 +0100

    Insert test

--- Backend/backend.py
+++ Backend/backend.py
@@ -6,14 +6,41 @@ base_dir = Path(__file__).parent
 database_dir = base_dir.parent / "Database"
 db_path = database_dir / "Simon_Stevin_sticker_tracker.db"
 
+current_user_id = "user123"
+current_latitude = 51.451
+current_longitude = 5.481
+current_date_picture = "2026-03-26"
+current_date_uploaded = "2026-03-26"
+current_committee = "media"
+current_title = "Campus Event"
+current_description = "Picture taken during event"
+
 def insert(db_path):
     connection = sqlite3.connect(db_path)
     cursor = connection.cursor()
 
-    cursor.execute(
+    cursor.execute("""
+    INSERT INTO sticker_data (
+        user_id,
+        latitude,
+        longitude,
+        date_picture,
+        date_uploaded,
+        committee,
+        title,
+        description
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    """, (
+        current_user_id,
+        current_latitude,
+        current_longitude,
+        current_date_picture,
+        current_date_uploaded,
+        current_committee,
+        current_title,
+        current_description
 
-
-    )
+    ))
     connection.commit()
     connection.close()
     return
