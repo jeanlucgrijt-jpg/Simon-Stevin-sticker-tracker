5ce594902508e42677a8accacc4bd5d4a73fbca5
Jean-Luc Grijt <j.c.a.grijt@student.tue.nl>
2026-05-02 17:57:49 +0200

    Frontend first adjustments

---
+++ Frontend/src/StickersList.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+const StickerList = ({stickers}) =>{
+    return <div>
+        <h2>Stickers</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>User id</th>
+                    <th>Latitude</th>
+                    <th>Longitude</th>
+                    <th>Picture date</th>
+                    <th>Sticker type</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+}
